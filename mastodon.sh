9c813b67c57893f9fd6e7344ca1643c0a1a392f3
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2025-03-08 10:28:20 +0100

    Working command line generation

--- mastodon.sh
+++ mastodon.sh
@@ -4,4 +4,5 @@ docker run --rm \
            --env-file .env \
            -v `pwd`/claimbot-server/accepted:/app/inbox \
           -it hochstenbach/mastodon-cli:v0.0.1 \
-          "$@" --account-id "http://localhost:3002/profile/card#me" 
+          "$@" --account-id "https://research.test.edu.nl/@alsvanounds" \
+               --account-name "Fetze Alsvanouds"

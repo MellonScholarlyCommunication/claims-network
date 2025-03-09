5ceff88f8e358cef97c7abc00d44da5496e0e65e
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2025-03-09 10:15:13 +0100

    Addding profile

--- mastodon.sh
+++ mastodon.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-docker run --rm \
+if [ "$1" == "generate" ]; then
+     docker run --rm \
            --env-file .env \
            -v `pwd`/claimbot-server/accepted:/app/inbox \
-          -it hochstenbach/mastodon-cli:v0.0.1 \
-          "$@" --account-id "https://research.test.edu.nl/@alsvanounds" \
-               --account-name "Fetze Alsvanouds"
+           -it hochstenbach/mastodon-cli:v0.0.1 \
+           "$@" --account-id "http://host.docker.internal:80/mastodon/alsvanouds/index.html" \
+                --account-name "Fetze Alsvanouds"
+else
+     docker run --rm \
+           --env-file .env \
+           -v `pwd`/claimbot-server/accepted:/app/inbox \
+           -it hochstenbach/mastodon-cli:v0.0.1 \
+           "$@" 
+fi
+
